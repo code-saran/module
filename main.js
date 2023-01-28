eedf1a4804dc0ba8f2386a726d213652c0dc1907
saranya <112763017+Saran-juno@users.noreply.github.com>
2023-01-28 11:09:34 +0530

    inserting javascript

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-    
+    <!DOCTYPE html>
+       <html>
+       <body>
+       <h2>My first javascript</h2>
+<button type="button" onclick="document.getElementById('demo').innerHTML=Dtae()">
+   click me to display Dtae and Time.</button>
+<p id="demo"></p>
+</body>
