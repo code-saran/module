e0647ddb4b9e162eadac9c0c9651fd55a74c501b
saranya <112763017+Saran-juno@users.noreply.github.com>
2023-02-27 19:18:31 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-<html>
-       <body>
-       <h2>My first javascript</h2>
-<button type="button" onclick="document.getElementById('demo').innerHTML=Dtae()">
-   click me to display Dtae and Time.</button>
-<p id="demo"></p>
-</body>
-</html>
